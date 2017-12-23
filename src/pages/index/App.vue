57f25ed9baf2cdea5b49accf58fff2f55e9f2dad
dunizb <dunizb@outlook.com>
2017-12-24 02:46:57 +0800

    响应式

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -77,20 +77,26 @@
                 <img src="https://img14.360buyimg.com/n0/jfs/t2191/111/699154754/198998/32d7bfe0/5624b582Nbc01af5b.jpg" />
                 <a class="iconfont icon-buy1" href="https://store.dunizb.com" target="_blank"><strong>￥49，去看看</strong></a>
               </div>
-              <div class="info">
+              <a class="info" href="https://store.dunizb.com" target="_blank">
+                <div class="buy_link">
+                  <a class="iconfont icon-webclicksettings" href="https://store.dunizb.com" target="_blank">￥49，去看看</a>
+                </div>
                 <h3>Java编程思想（第4版）[thinking in java]</h3>
                 <span>全球程序员广泛赞誉 Java扛鼎经典之作</span>
-              </div>
+              </a>
             </div>
             <div class="item">
               <div class="item_img">
                 <img src="https://img14.360buyimg.com/n0/jfs/t3136/71/355944483/202149/2788207/57b2f0d5N776838af.jpg" />
                 <a class="iconfont icon-buy1" href="https://store.dunizb.com" target="_blank"><strong>￥20，去看看</strong></a>
               </div>
-              <div class="info">
+              <a class="info" href="https://store.dunizb.com" target="_blank">
+                <div class="buy_link">
+                  <a class="iconfont icon-webclicksettings" href="https://store.dunizb.com" target="_blank">￥49，去看看</a>
+                </div>
                 <h3>内外兼修：程序员的成长之路</h3>
                 <span>如何从一个普通程序员成长为架构师、团队负责人</span>
-              </div>
+              </a>
             </div>
           </div>
         </div>
@@ -102,7 +108,7 @@
         <div class="content">
           <div id="my-maps"></div>
           <div class="row_btn">
-            <a href="https://dunizb.github.io/footprint" target="_blank"><i class="iconfont icon-footerx"></i> 查看详细足迹 <i class="iconfont icon-footerx"></i></a>
+            <a href="https://dunizb.github.io/footprint" target="_blank"><i class="iconfont icon-webclicksettings"></i> 查看详细足迹</a>
           </div>
         </div>
       </div>
