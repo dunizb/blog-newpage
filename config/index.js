deb150d2c80891fe2c94c3d251ea7136978978cb
dunizb <dunizb@outlook.com>
2017-12-25 20:07:54 +0800

    Build.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '.', // 写成每个项目的绝对路径(修改)
+    assetsPublicPath: '', // 写成每个项目的绝对路径(修改)
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
