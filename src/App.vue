916838112d582c078738bca54fc5c6fea1f217b6
Dunizb <zb@dunizb.vip>
2020-03-26 13:44:09 +0800

    Vue3

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,36 @@
             <i class="iconfont icon-article1"></i> 博客
           </a>
         </li>
+        <li title="Vue3备忘单">
+          <a
+            class="store"
+            onclick="_hmt.push(['_trackEvent', 'nav', 'click', 'Vue3备忘单'])"
+            href="/vue3/"
+          >
+            <svg
+              style="vertical-align: middle;"
+              t="1572367313845"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="6427"
+              width="16"
+              height="16"
+            >
+              <path
+                d="M615.6 123.6h165.5L512 589.7 242.9 123.6H63.5L512 900.4l448.5-776.9z"
+                fill="#41B883"
+                p-id="6428"
+              />
+              <path
+                d="M781.1 123.6H615.6L512 303 408.4 123.6H242.9L512 589.7z"
+                fill="#34495E"
+                p-id="6429"
+              />
+            </svg> Vue3
+          </a>
+        </li>
         <li title="我的小铺">
           <a
             class="store"
