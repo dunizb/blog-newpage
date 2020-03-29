d099629dfa7c18bd189f667605db249694443e60
Dunizb <zb@dunizb.vip>
2020-03-29 23:17:48 +0800

    update vue3

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  var stylesheet = document.getElementById("stylesheet"),
+    light = document.getElementById("toggle-light"),
+    dark = document.getElementById("toggle-dark");
+  light.addEventListener("click", function() {
+    setTheme("light");
+  });
+  dark.addEventListener("click", function() {
+    setTheme("dark");
+  });
+
+  function setTheme(theme) {
+    console.log(theme);
+    stylesheet.href = "css/" + theme + ".css";
+  }
+};
